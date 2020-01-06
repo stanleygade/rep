116f2d3cf64e2a86ae540aed45eed5214867c06f
Søren Gade <stanley@zimba.dk>
2020-01-06 15:55:00 +0100

    vhmxgfy

---
+++ Leg1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Leg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = System.DateTime.Now;
+            DateTime dt2, dt3;
+            Console.WriteLine($"dt - Systemtid : {dt}");
+            dt = dt.AddDays(10);
+            Console.WriteLine($"dt: {dt}");
+            dt = dt.AddMinutes(20);
+            Console.WriteLine($"dt: {dt}");
+            dt = dt.AddMonths(-1);
+            Console.WriteLine($"dt: {dt}");
+            DateTime dt4 = new DateTime(1972, 12, 2, 12, 2, 1);
+            Console.WriteLine($"dt4: {dt4}");
+
+        }
+    }
+}
